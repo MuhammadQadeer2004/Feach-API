804b31fd4281dd36785ba8368c6b564f7ac189b1
Muhammad Qadeer <qadeer142004@gmail.com>
2025-09-26 02:23:49 +0500

    first commit

--- src/StudentTable.js
+++ src/StudentTable.js
@@ -1,12 +1,22 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { data, Link } from "react-router-dom";
 
 export default function StudentTable() {
+
+  const [student, setStudent] = useState("");
+
+  useEffect(() => {
+    fetch('http://localhost:8000/studentdata')
+      .then((res) => res.json())
+      .then((data) =>
+        setStudent(data)).catch((err) =>
+          console.log(err.message))
+  }, [])
   return (
     <div className='container' >
       <h2>Student Records</h2>
       <div className="table-container">
-        {/* <a href="#" class="btn btn-add">Add new Student</a> */}
-        <Link to="/student/create" class="btn btn-add">Add new Student</Link>
+        <Link to="/student/create" className="btn btn-add">Add new Student</Link>
         <table>
           <thead>
             <tr>
@@ -17,17 +27,22 @@ export default function StudentTable() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Qadeer</td>
-              <td>Lahore</td>
-              <td>757577848</td>
-              <td>
-                <a href="" className="btn btn-info">View</a>
-                <a href="" className="btn btn-primary">Edit</a>
-                <a href="" className="btn btn-danger">Delete</a>
-              </td>
-            </tr>
+            {
+              student && student.map((item) => (
+                <tr>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.place}</td>
+                  <td>{item.phone}</td>
+                  <td>
+                    <a href="" className="btn btn-info">View</a>
+                    <a href="" className="btn btn-primary">Edit</a>
+                    <a href="" className="btn btn-danger">Delete</a>
+                  </td>
+                </tr>
+              )
+              )
+            }
           </tbody>
         </table>
       </div>
